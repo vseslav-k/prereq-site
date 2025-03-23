b3cb0e9b6971f2b51b5a9b0972b04aacd5166b09
vseslav-k <vseslavvak@mail.ru>
2025-03-23 00:31:23 -0700

    redeploy site pls

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -8,5 +8,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  res.status(200).json({ message: 'Slavjanskij pinok jasheram' });
+  res.status(200).json({ message: 'Slavjanskij pinok jasheram!!!!' });
 }
